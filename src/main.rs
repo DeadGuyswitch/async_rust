86d32723e2f93210aa09bfb90a7a9e83978a6d11
DeadGuyswitch <nirdesh.ghost@gmail.com>
2022-07-22 20:08:10 +0545

    Synchronous programming with multiple delays in functions

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,26 @@
+use std::thread::sleep;
+use std::time::Duration;
+
 fn main() {
-   println!("Before reading the file");
-   let file_contents = read_from_file();
-   println!("{:?}", file_contents);
-   println!("Hello after reading file");
+   
+    println!("Before reading the file");
+   
+   let file1_contents = read_from_file1();
+   println!("{:?}", file1_contents);
+   println!("Hello after reading file1");
+
+   let file2_contents = read_from_file2();
+   println!("{:?}", file2_contents);
+   println!("Hello after reading file2");
 }
 
-fn read_from_file() -> String {
-    String::from("Hello World")
+fn read_from_file1() -> String {
+    sleep(Duration::new(4,0));
+    String::from("Hello World from file 1")
 }
+
+fn read_from_file2() -> String {
+    sleep(Duration::new(2,0));
+    String::from("Hello World from file 2")
+}
+
